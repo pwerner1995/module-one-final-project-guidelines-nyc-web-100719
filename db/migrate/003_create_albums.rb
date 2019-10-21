c4bbcafc30707b0900de9ea530afedc8bc1e3a20
Bailey Wood <woodbmae@gmail.com>
2019-10-21 15:11:13 -0400

    migrate .time in reviews table and .title in albums table

--- db/migrate/003_create_albums.rb
+++ db/migrate/003_create_albums.rb
@@ -2,6 +2,7 @@ class CreateAlbums < ActiveRecord::Migration[5.1]
     def change
         create_table :albums do |t|
             t.integer :release_year
+            t.string :title
 
             t.timestamps
         end
