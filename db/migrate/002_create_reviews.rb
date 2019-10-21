8b1a72e6a590b65265d947671525520ff8af1e09
github username <github email address>
2019-10-21 14:31:28 -0400

    Add reviews table for Review class

---
+++ db/migrate/002_create_reviews.rb
@@ -0,0 +1,12 @@
+class CreateReviews < ActiveRecord::Migration[5.1]
+    def change
+        create_table :reviews do |t|
+            t.datetime :date
+            t.integer :album_id
+            t.integer :user_id
+            t.text :review_content
+
+            t.timestamps
+        end
+    end
+end
