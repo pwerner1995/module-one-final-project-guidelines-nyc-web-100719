91496b4f532b5624c24dfc1d38b3b9937256696a
github username <github email address>
2019-10-22 12:42:47 -0400

    Add app file to run tty-prompt methods

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,6 @@
 require_relative '../config/environment'
 
-
+my_review= MyReview.new
+my_review.run
 
 puts "HELLO WORLD"
---
+++ lib/my_review.rb
@@ -0,0 +1,10 @@
+require 'tty-prompt'
+class MyReview 
+    def run  
+        prompt_user
+    end
+
+    def prompt_user
+        puts "hey"
+    end
+end
