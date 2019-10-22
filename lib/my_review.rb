b6c8b8faa17c23f93b613967dfee8599f9944d78
github username <github email address>
2019-10-22 13:15:24 -0400

    Add prompt_user and prompt_password methods to MyReview class

--- lib/my_review.rb
+++ lib/my_review.rb
@@ -1,10 +1,22 @@
 require 'tty-prompt'
+require 'pry'
 class MyReview 
     def run  
-        prompt_user
+        user_name=prompt_user
+        password=prompt_password
+        binding.pry
     end
 
+    def prompt
+        TTY::Prompt.new
+    end
     def prompt_user
-        puts "hey"
+        puts "Welcome to Bete's by Flatiron!!"
+        prompt.ask('Enter your username:')
+        gets.chomp!
+    end
+    def prompt_password
+        prompt.mask('Enter password:')
+        gets.chomp!
     end
 end
